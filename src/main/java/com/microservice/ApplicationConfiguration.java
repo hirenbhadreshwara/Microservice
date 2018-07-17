e1e0a15a1a6ffe4384dc73667673e801d62abfc4
hiren.bhadreshwara <hiren@ICWS-1197>
2018-07-17 10:03:32 +0530

    add null check

--- src/main/java/com/microservice/ApplicationConfiguration.java
+++ src/main/java/com/microservice/ApplicationConfiguration.java
@@ -2,6 +2,7 @@ package com.microservice;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.common.WebServiceResponse;
@@ -11,11 +12,13 @@ import com.common.WebServiceResponse;
 public class ApplicationConfiguration {
 
 	@RequestMapping("/hello")
-	public WebServiceResponse hello(@RequestParam("name") String name) {
+	public @ResponseBody WebServiceResponse hello(@RequestParam("name") String name) {
 		WebServiceResponse res = new WebServiceResponse();
-		res.setReturnObj(name);
-		res.setSucess(true);
-		System.out.println("name : "+name);
+		if(!name.equalsIgnoreCase("")) {
+			res.setReturnObj(name);
+			res.setSucess(true);
+			System.out.println("name : "+name);
+		}
 		return res;
 	}
 }
